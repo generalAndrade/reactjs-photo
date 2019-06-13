d6e670aabac1d852c7d14cb41e1638bf9abdad7f
Leonardo Gerheim de Andrade <general.andrade@gmail.com>
2019-06-13 17:18:56 -0300

    Updates

---
+++ precache-manifest.bc282198db7489b8b023b8c568e61052.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c55d4b461a331f61e3ecb105432947",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73910603946fbf8cb1e3",
+    "url": "/static/css/main.aa345f97.chunk.css"
+  },
+  {
+    "revision": "7a4c94f8851360d6f863",
+    "url": "/static/js/2.ef8208e9.chunk.js"
+  },
+  {
+    "revision": "73910603946fbf8cb1e3",
+    "url": "/static/js/main.115b26fb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "787c6af98ac770e43e65815a8ff337df",
+    "url": "/static/media/camera.787c6af9.svg"
+  },
+  {
+    "revision": "e2a499354e3b1d309ff71584d7290d29",
+    "url": "/static/media/comment.e2a49935.svg"
+  },
+  {
+    "revision": "00e13584091199bddd3785adaf0b9f3d",
+    "url": "/static/media/like.00e13584.svg"
+  },
+  {
+    "revision": "22d960a9ac06cc252579791795bdea8c",
+    "url": "/static/media/more.22d960a9.svg"
+  },
+  {
+    "revision": "55e74cfdc1470a74c5bc8c2af6ed9fa0",
+    "url": "/static/media/send.55e74cfd.svg"
+  }
+]);
